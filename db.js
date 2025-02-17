cb2ec6eff497862fbed36d45b044e35ef0bcacab
vishal <vishalverma@gmail>
2025-02-17 23:13:35 +0530

    first version of OnlineBookStore

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 require('dotenv').config();
 
-//const mongoURL='mongodb://localhost:27017/onlinebookstore';
+
 const mongoURL = process.env.MONGODB_URL_LOCAL
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
